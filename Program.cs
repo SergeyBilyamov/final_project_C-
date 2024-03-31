257230310b8ac372acc9246e72e44031a871d556
Sergey <151991545+SergeyBilyamov@users.noreply.github.com>
2024-03-31 15:50:37 +0500

    Добавлена программа итогового проэкта

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿string[] sourceArray = FillArray();
+string[] resultArray = GenerateNewArray(sourceArray);
+string firstArray = PrintArray(sourceArray);
+string secondArray = PrintArray(resultArray);
+Console.WriteLine(firstArray + " -> " + secondArray);
+
+string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] sourceArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return sourceArray;
+}
+
+string PrintArray(string[] sourceArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < sourceArray.Length; i++)
+    {
+        if (i == sourceArray.Length - 1)
+        {
+            stringArray += $"\"{sourceArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{sourceArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
+
+int CountStringSymbols(string[] sourceArray)
+{
+    int counter = 0;
+    foreach (string item in sourceArray)
+    {
+        if (item.Length <= 3)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+string[] GenerateNewArray(string[] sourceArray)
+{
+    int resultArrayLength = CountStringSymbols(sourceArray);
+    string[] resultArray = new string[resultArrayLength];
+    int i = 0;
+    foreach (string item in sourceArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[i] = item;
+            i++;
+        }
+    }
+    return resultArray;
+}
